51f491e654cbe79756dd718ac895edb126fe5706
darth-vader-lg <luigi.generale@gmail.com>
2023-07-11 15:10:21 +0200

    First test

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using SmartCL;
 
 // Operational buffer
@@ -18,9 +19,76 @@ for (var i = 0; i < a.Length; i++) {
 if (CL.DefaultDevice is var device && device is null)
     throw new Exception("Cannot find a default device");
 
-// Quick operation on an array
-a.ExecuteOnDevice(device, new[]
+
+
+
+// Create a program running on GPU
+var rnd = new Random();
+var ppc = 720;
+var vertices = device.Context.CreateBuffer<Vect>(1024, CLAccess.WriteOnly);
+var result = device.Context.CreateBuffer<int>(ppc, CLAccess.ReadOnly);
+device.Context.CreateProgram(new[]
 {
+    @"typedef struct Vect_t {" +
+    @"   double x;",
+    @"   double y;",
+    @"   double z;",
+    @"} Vect;",
+    @"",
+    @"",
+    @"kernel void main(global Vect* vertices, global int* results) {",
+    $"   Vect vTemp[{vertices.Length}];",
+    @"   global int* result = &results[get_global_id(0)];",
+    @"   *result = -1;",
+    @"   double dMin = 1E6;",
+    $"   for (int i = 0; i < {vertices.Length}; i++) {{",
+    @"      Vect* v = &vTemp[i];",
+    @"      *v = vertices[i];",
+    @"      double d = sqrt(v->x * v->x + v->y * v->y + v->z * v->z);",
+    @"      if (d < dMin) {",
+    @"         *result = i;",
+    @"         dMin = d;",
+    @"      }",
+    @"   }",
+    @"}"
+});
+using (var kernel = device.CreateKernel("main", vertices.AsCLArg(), result.AsCLArg())) {
+    kernel.GlobalSizes = new[] { ppc };
+    using (var map = kernel.Map(vertices, CLAccess.WriteOnly)) {
+        for (var i = 0; i < map.Length; i++)
+            map[i] = new Vect { x = rnd.NextDouble() * 1000, y = rnd.NextDouble() * 1000, z = rnd.NextDouble() * 1000 };
+    }
+    var timer = new Stopwatch();
+    kernel.Invoke();
+    timer.Start();
+    for (var i = 0; i < 200; i++) {
+        kernel.Invoke();
+    }
+    timer.Stop();
+    Console.WriteLine($"Total ms {timer.Elapsed.TotalMilliseconds}");
+    int min = -1;
+    using (var map = kernel.MapRead(result)) {
+        min = map[0];
+    }
+    min = -1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Quick operation on an array
+    a.ExecuteOnDevice(device, new[]
+    {
     "__kernel void main(__global int* A, const int c) {",
     "   A[get_global_id(0)] = c * A[get_global_id(0)];",
     "}"
@@ -81,3 +149,11 @@ using (var map = fillHost.MapRead(fillHost.Arg0)) {
 
 // Delegate for kernel function
 delegate void KernelFunction(int[] values, int mul);
+
+[StructLayout(LayoutKind.Sequential)]
+struct Vect
+{
+    public double x;
+    public double y;
+    public double z;
+}
