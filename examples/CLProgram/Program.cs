b4eac9349b69e7dd1ef05d5679c1ac88455557cc
darth-vader-lg <luigi.generale@gmail.com>
2023-07-15 00:40:14 +0200

    Add CLProgram (program for testing kernels' emulation code)

---
+++ examples/CLProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using static CLKernels;
+
+Console.WriteLine("Hello, World!");
+unsafe {
+    var data = stackalloc int[] { 123 };
+    main(data); 
+}
+
+static class CLKernels
+{
+    [DllImport("CLKernels", EntryPoint = "_main")]
+    public extern static unsafe void main(int* data);
+}
